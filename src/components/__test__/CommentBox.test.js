a5a255774e267c7fd572658264a2c2779a0c1b8a
Shankhadeep1234 <shankhadeepraj@gmail.com>
2020-08-02 23:25:47 +0530

    added describ block to grp and beforeEach to extract similar code block

--- src/components/__test__/CommentBox.test.js
+++ src/components/__test__/CommentBox.test.js
@@ -18,15 +18,22 @@ it("has a textarea and button", () => {
   expect(wrapped.find("button").length).toEqual(1);
 });
 
-it("has a text area that users can type in", () => {
-  wrapped.find("textarea").simulate("change", {
-    target: { value: "new comment" },
+describe("the text area", () => {
+  beforeEach(() => {
+    wrapped.find("textarea").simulate("change", {
+      target: { value: "new comment" },
+    });
+
+    wrapped.update();
   });
 
-  wrapped.find("form").simulate("submit");
+  it("has a text area that users can type in", () => {
+    expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
+  });
 
-  wrapped.update();
-
-  expect(wrapped.find("textarea").prop("value")).toEqual("");
-  // expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
+  it("when form is submitted textarea gets empty", () => {
+    wrapped.find("form").simulate("submit");
+    wrapped.update();
+    expect(wrapped.find("textarea").prop("value")).toEqual("");
+  });
 });
